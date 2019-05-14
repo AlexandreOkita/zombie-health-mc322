246cba79c63fd3341d44a029f20fe9c50e5a42c1
Sinara C. Pamplona <50603935+SinaraP@users.noreply.github.com>
2019-05-14 15:12:10 -0300

    Modificando IDoctor

--- src/project/interfaces/IDoctor.java
+++ src/project/interfaces/IDoctor.java
@@ -1,5 +1,6 @@
 package project.interfaces;
 
 public interface IDoctor extends IEnquirer, IAnswerReceptacle, ITableProducerReceptacle, ITreeProducerReceptacle{
-    
+    public void connect(IAnswer responder);
+    public void connect(ITreeProducer tree);
 }
