5a76dc5b99be25f4c7c0f838948d211e0e1211df
Alexandre Okita <37855969+AlexandreOkita@users.noreply.github.com>
2019-06-01 23:07:48 -0300

    Add files via upload

--- src/project/API/api_teste.py
+++ src/project/API/api_teste.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify, request
+from urllib.request import urlopen, Request
+import os
 
+app = Flask(__name__)
+@app.route('/datasets', methods=['GET'])
+def teste():
+    return "Certo!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port = port)
+
+return jsonify('teste')
