cc4a4eaa36fc18d36dded9dd11cebbef210933b2
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-06 15:23:57 -0300

    Update Patient.java

--- src/dinoscientist/components/Patient.java
+++ src/dinoscientist/components/Patient.java
@@ -2,10 +2,34 @@ package dinoscientist.components;
 
 import dinoscientist.interfaces.IClient;
 
-public class Patient implements IClient {
+// public interface IDataSet
+// {
+//     public String[] getSintoma();
+// }
 
-    @Override
-    public int test() {
-        return 1;
+// public class DataSet implements IDataSet{
+//     public String[] getSintoma()
+//     {
+//         return null; // (1) retorna vetor randômico de strings da doença
+//     }
+// }
+
+public class Patient implements IClient{
+
+    private String[] disease;
+    private String disease_name;
+
+    private IDataSet DataSet;
+
+    public Patient(){
+        this.disease = DataSet.getSintoma();
+    }
+
+    public void DataSetConnect( IDataSet data ){
+        this.DataSet = data;    // (2) pegue um sintoma randômico
+    }
+
+    public String getDoencaName(){
+        return disease_name; // 0 terá string
     }
 }
