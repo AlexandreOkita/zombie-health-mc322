9b9c057f6096f1b2acfaa30109d36cff94085f96
Sinara C. Pamplona <50603935+SinaraP@users.noreply.github.com>
2019-06-06 15:43:16 -0300

    pequenas modificações

--- src/dinoscientist/components/CRM.java
+++ src/dinoscientist/components/CRM.java
@@ -2,6 +2,5 @@ package dinoscientist.components;
 
 import dinoscientist.interfaces.IComponent;
 
-public abstract class CRM implements IComponent{
-    //TODO 06/06/19 tirar a classe abstrata e colocar como interface
+public class CRM implements ICRM, IObserver{
 }
