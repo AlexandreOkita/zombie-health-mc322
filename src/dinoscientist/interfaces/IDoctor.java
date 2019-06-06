5ffa78721b4530be91f3c5534be5cda429a4c012
Sinara C. Pamplona <50603935+SinaraP@users.noreply.github.com>
2019-06-06 15:44:41 -0300

    changing names

--- src/dinoscientist/interfaces/IDoctor.java
+++ src/dinoscientist/interfaces/IDoctor.java
@@ -1,5 +1,5 @@
 //Observer Pattern - Subject
 public interface IDoctor extends IComponent{
   public String diagnostic(IPatient pat, ArrayList...) //firstopinion, ARRUMAR PARAMETROS
-  public String askOpinion2(ICRM conselho, IPatient pat); //notify ~ second_opinion
+  public String askOpinion2(ICRM council, IPatient pat); //notify ~ second_opinion
 }
