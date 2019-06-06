46ab6b67e35c8c9f2e4aeb873245bc20b1bd4669
Sinara C. Pamplona <50603935+SinaraP@users.noreply.github.com>
2019-06-06 15:34:52 -0300

    Create IDoctor.java

---
+++ src/dinoscientist/interfaces/IDoctor.java
@@ -0,0 +1,5 @@
+//Observer Pattern - Subject
+public interface IDoctor extends IComponent{
+  public String diagnostic(IPatient pat, ArrayList...) //firstopinion, ARRUMAR PARAMETROS
+  public String askOpinion2(ICRM conselho, IPatient pat); //notify ~ second_opinion
+}
