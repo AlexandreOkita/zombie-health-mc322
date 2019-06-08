1309d957c0e2e8450d094092227d92b255081a20
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-07 21:30:13 -0300

    Paciente final
    Depende do DataSet para geração de sintomas.

--- src/dinoscientist/components/Patient.java
+++ src/dinoscientist/components/Patient.java
@@ -1,35 +1,35 @@
 package dinoscientist.components;
 
 import dinoscientist.interfaces.IClient;
-
-// public interface IDataSet
-// {
-//     public String[] getSintoma();
-// }
-
-// public class DataSet implements IDataSet{
-//     public String[] getSintoma()
-//     {
-//         return null; // (1) retorna vetor randômico de strings da doença
-//     }
-// }
+import java.util.Random;
 
 public class Patient implements IClient{
 
-    private String[] disease;
-    private String disease_name;
+    private int[] disease;
 
     private IDataSet DataSet;
 
-    public Patient(){
-        this.disease = DataSet.getSintoma();
+    public void dataSetConnect( IDataSet data ){
+        this.DataSet = data;    // (2) pegue um sintoma randômico;
+    }
+    
+    public void generateDisease(){
+        
+        int n_diseases;
+        String sintomas[] = DataSet.requestAttributes();
+        for ( n_diseases = 0; n_diseases < sintomas.length; n_diseases++ ){} // Incrementador de n_diseases.
+        
+        Random rand = new Random();
+        
+        this.disease = new int[n_diseases];
+        
+        for ( int idx = 0 ; idx < n_diseases ; idx++ ){
+            disease[idx] = rand.nextInt(2);
+            System.out.println(disease[idx]);
+        }
     }
 
-    public void DataSetConnect( IDataSet data ){
-        this.DataSet = data;    // (2) pegue um sintoma randômico
-    }
-
-    public String getDoencaName(){
-        return disease_name; // 0 terá string
+    public int[] getSintomas(){
+        return this.disease; // 0 terá string
     }
 }
