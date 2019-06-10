a94dbf9ebf0d23989bc92dd835cac85d12c8f4e1
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-10 00:23:59 -0300

    Novo método setDataSource
    Adicionado método setDataSource para adequar ao padrão de fábricas

--- src/dinoscientist/interfaces/IDataSet.java
+++ src/dinoscientist/interfaces/IDataSet.java
@@ -5,4 +5,5 @@ import dinoscientist.interfaces.IComponent;
 public interface IDataSet extends IComponent{
     public String[] requestAttributes();
     public String[][] requestInstances();
+    public void setDataSource(String dataSource );
 }
