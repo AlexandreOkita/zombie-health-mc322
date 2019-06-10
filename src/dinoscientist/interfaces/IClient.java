2e2b264fe9f2d58f8b7eed7d3e25bf86883b7629
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-10 00:22:52 -0300

    Update IClient.java
    Agora retorna ArrayList<int> em getSintomas ao invés de int[]

--- src/dinoscientist/interfaces/IClient.java
+++ src/dinoscientist/interfaces/IClient.java
@@ -4,6 +4,7 @@ import dinoscientist.interfaces.IComponent;
 
 public interface IClient extends IComponent{
     public void dataSetConnect( IDataSet data );
-    public void generateDisease();
-    public int[] getSintomas();
+    public void setDisease( int n_diseases );
+    public ArrayList<Integer> getSintomas();
+    public int getN_sintomas();
 }
