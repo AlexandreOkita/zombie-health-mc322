32fa04d18a1058b8140fb13f6128a67bbeeb9dc7
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-11 01:08:46 -0300

    Update ICRM.java

--- src/dinoscientist/interfaces/ICRM.java
+++ src/dinoscientist/interfaces/ICRM.java
@@ -3,5 +3,4 @@ package dinoscientist.interfaces
 import dinoscientis.interfaces.IObserver;
 
 public interface ICRM extends IObserver, IComponent{
-    public List<String> update( List<Intenger> respostas );
 }
