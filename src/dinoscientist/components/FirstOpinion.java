d7cce1c534b62653ed5d29e125f79879f531342c
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-10 22:59:33 -0300

    Create FirstOpinion.java

---
+++ src/dinoscientist/components/FirstOpinion.java
@@ -0,0 +1,17 @@
+package dinoscientist.components.FirstOpinion;
+
+import dinoscientist.interfaces.IFirst;
+import dinoscientist.interfaces.IComponent;
+
+public class FirstOpinion extends IFirst{
+    Question question;
+
+    public connect ( Question question ){
+        this.question = question;
+    }
+
+    public List<SintomaDoenca> getBestQuestion( String dataset ){
+        return this.question.list(String dataset);
+    }
+}
+
