97a2df3aaa57e57c2825aa71694f5e61c98578c4
Sinara C. Pamplona <50603935+SinaraP@users.noreply.github.com>
2019-06-10 21:34:25 -0300

    update
    lembrar que na main precisa dar um attach(crm) para utilizar a funcao diagnostic e notify

--- src/dinoscientist/components/Doctor.java
+++ src/dinoscientist/components/Doctor.java
@@ -4,10 +4,10 @@ public class Doctor implements IDoctor, ISubject{
   private ICRM council;
   private IPatient patient;
 
-  public Doctor(String name, ICRM crm){
+  public Doctor(String name){
     this.name = name;
     f_opinion = null;
-    council = crm; //precisa no construtor?
+    council = null;
     patient = null;
   }
   
@@ -18,13 +18,13 @@ public class Doctor implements IDoctor, ISubject{
   public String diagnostic(IPatient pat, ArrayList<SintomaDoenca> questions){ //ARRUMAR PARAMETROS
     String opinion;
     int answer;
-    ArrayList<String> others_opinions;
+    List<String> others_opinions;
     
     connect(pat);
     
     System.out.println("Doctor: Hello, "+pat.getName()+ " I'm "+name);
     
-    ArrayList<String> answers = new ArrayList<String>();
+    List<String> answers = new ArrayList<String>(); //ta certo o tipo okita?
     
     for(SintomaDoenca ques: questions){
       System.out.println(ques+" ?");
@@ -50,10 +50,7 @@ public class Doctor implements IDoctor, ISubject{
     else{
       
       System.out.println(pat.getName()+" is not getting better :(");
-      System.out.println("I will check with the council");
-      
-      attachCRM(crm);
-      
+      System.out.println("I will check with the council");      
       others_opinions = notifyCRM(pat);
       System.out.println("Then, it is "+others_opinions.get(1));
       System.out.println("**Taking care of patient (again)**")
