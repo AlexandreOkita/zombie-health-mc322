7b34b7254ff0f3cb05326154e1bfebb81a5e7926
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-11 04:24:30 -0300

    Update IDoctor.java

--- dinosci_zombiedoc/src/dinoscientist/interfaces/IDoctor.java
+++ dinosci_zombiedoc/src/dinoscientist/interfaces/IDoctor.java
@@ -1,5 +1,5 @@
 //ObserverPattern - Subject
 public interface IDoctor extends IComponent{
   public String diagnostic(ArrayList<SintomaDoenca> questions); //ESTAO CERTOS OS PARAMETROS?
-  public List<String> notifyCRM(IPatient pat, List<Integer> respostas); //notify do pattern
+  public List<String> notifyCRM(List<Integer> respostas); //notify do pattern
 }
