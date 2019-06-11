2791545288b60d946994a5df4d218cf4be597b14
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-10 21:50:19 -0300

    Update CRM.java

--- src/dinoscientist/components/CRM.java
+++ src/dinoscientist/components/CRM.java
@@ -3,4 +3,14 @@ package dinoscientist.components;
 import dinoscientist.interfaces.IComponent;
 
 public class CRM implements ICRM, IObserver{
+    
+    Examine examine;
+  
+    public CRM( Examine examine){
+        this.examine = examine;
+    }
+    
+    public List<String> update( List<Intenger> respostas ){
+        return this.examine.execute(dataset, respostas);
+    }
 }
