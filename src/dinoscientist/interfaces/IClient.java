3d3e7c85cddce8404a1ae1453c8dd2f1c3dd9468
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-11 04:07:54 -0300

    Update IClient.java

--- src/dinoscientist/interfaces/IClient.java
+++ src/dinoscientist/interfaces/IClient.java
@@ -3,7 +3,6 @@ package dinoscientist.interfaces;
 import dinoscientist.interfaces.IComponent;
 
 public interface IClient extends IComponent{
-    pubic void connectCRM ( ICRM crm );
     public void connectDataSet( IDataSet data );
     public void setDisease( int n_diseases );
     public int getSintoma( String key );
