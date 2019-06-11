2c0e220985e135aab68be2582eccf8184dfb9f75
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-11 12:01:35 -0300

    Update IClient.java

--- dinosci_zombiedoc/src/dinoscientist/interfaces/IClient.java
+++ dinosci_zombiedoc/src/dinoscientist/interfaces/IClient.java
@@ -5,6 +5,7 @@ import dinoscientist.interfaces.IComponent;
 public interface IClient extends IComponent{
     public void connectDataSet( IDataSet data );
     public void setDisease();
+    public List<Integer> getSymp();
     public int getSintoma( String key );
     public String getName();
 }
