90157486ff218b2267a1f23d6fe9dcbb819a8b01
Alexandre Okita <37855969+AlexandreOkita@users.noreply.github.com>
2019-06-11 13:17:20 -0300

    Update ComponentFactory.java

--- src/dinoscientist/factories/ComponentFactory.java
+++ src/dinoscientist/factories/ComponentFactory.java
@@ -1,7 +1,8 @@
 package dinoscientist.factories;
 
 import dinoscientist.interfaces.IComponent;
+import dinoscientist.interfaces.IComponentFactory;
 
-public abstract class ComponentFactory{
+public abstract class ComponentFactory implements IComponentFactory{
     public abstract IComponent getComponent();
 }
